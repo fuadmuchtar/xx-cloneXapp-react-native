088a358b44d792114ed2dd6be069a47cd29625c4
fuadmuchtar <tech.fuad99@gmail.com>
2025-05-13 16:17:57 +0700

    feat: add dotenv configuration for MongoDB connection and example .env file

---
+++ server/config/mongodb.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri)
+const database = client.db("p3-gc1")
+
+module.exports = { database }
