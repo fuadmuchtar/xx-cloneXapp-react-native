e941e9c5e042ac93646031575cf14419b2f746aa
fuadmuchtar <tech.fuad99@gmail.com>
2025-05-18 09:15:46 +0700

    feat: implement authentication scheme flow with WelcomeScreen and enhanced LoginScreen; add navigation logic and update dependencies

--- xx/navigators/StackNav.js
+++ xx/navigators/StackNav.js
@@ -1,12 +1,20 @@
 import { createStackNavigator } from "@react-navigation/stack"
 import LoginScreen from "../screens/LoginScreen"
+import WelcomeScreen from "../screens/WelcomeScreen"
 
 const Stack = createStackNavigator()
 export default function StackNav() {
+    const { isSignedIn } = false // Replace with your authentication logic
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Login" options={{ title: 'X' }} component={LoginScreen} />
-            {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+            {isSignedIn ? (
+                <Stack.Screen name="Home" component={HomeScreen} />
+            ) : (
+                <>
+                    <Stack.Screen name="Welcome" options={{ title: 'X' }} component={WelcomeScreen} />
+                    <Stack.Screen name="Login" component={LoginScreen} />
+                </>
+            )}
         </Stack.Navigator>
     )
 }
--- xx/screens/LoginScreen.js
+++ xx/screens/LoginScreen.js
@@ -1,38 +1,122 @@
+import { Feather } from "@expo/vector-icons";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function LoginScreen() {
-    const [email, setEmail] = useState("")
+    const [emailUsername, setEmailUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [isTrue, setIsTrue] = useState(false)
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const handleUser = async () => {
+        setIsTrue(true)
+    }
+
+    const handleLogin = async () => {
+        console.log("Login")
+    }
+
+    if (!isTrue)
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Text style={styles.title}>To get started, first enter your email or @username</Text>
+                </View>
+                <View style={{ flex: 1 }}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Email, or username"
+                        onChangeText={(emailUsername) => setEmailUsername(emailUsername)}
+                        value={emailUsername}
+                    />
+                </View>
+                <View style={{ alignItems: "flex-end" }}>
+                    <TouchableOpacity onPress={handleUser}>
+                        <Text style={styles.button}>Next</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+
     return (
         <View style={styles.container}>
-            <View style={{ flex: 2, justifyContent: "center", alignItems: "center" }}>
-                <Text style={{ fontSize: 30, textAlign: "center" }}>See what's happening in the world right now.</Text>
+            <View style={{}}>
+                <Text style={styles.title}>Enter your password</Text>
             </View>
             <View style={{ flex: 1 }}>
-                <TouchableOpacity>
-                    <Text style={styles.button}>Login</Text>
-                </TouchableOpacity>
-                <Text style={{ textAlign: "center" }}>or</Text>
-                <TouchableOpacity>
-                    <Text style={styles.button}>Register</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email, or username"
+                    onChangeText={(emailUsername) => setEmailUsername(emailUsername)}
+                    value={emailUsername}
+                />
+                <View>
+                    <View style={styles.passwordContainer}>
+                        <TextInput
+                            style={styles.passwordInput}
+                            placeholder="Password"
+                            value={password}
+                            onChangeText={setPassword}
+                            secureTextEntry={!passwordVisible}
+                        />
+                        <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)}>
+                            <Feather name={passwordVisible ? "eye" : "eye-off"} size={20} color="#657786" />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+            <View>
+                <TouchableOpacity onPress={handleLogin}>
+                    <Text style={{
+                        fontSize: 20,
+                        textAlign: "center",
+                        backgroundColor: "black",
+                        color: "#fff",
+                        padding: 15,
+                        borderRadius: 999,
+                    }}>Log in</Text>
                 </TouchableOpacity>
             </View>
         </View>
     )
 }
 
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#fff",
-        paddingHorizontal: 20
+        padding: 20
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: "bold",
+        marginTop: 10
+    },
+    input: {
+        marginTop: 50,
+        borderBottomWidth: 1,
+        fontSize: 16,
+        paddingVertical: 8,
     },
     button: {
         fontSize: 20,
+        width: 80,
         textAlign: "center",
         backgroundColor: "black",
         color: "#fff",
-        padding: 15,
+        padding: 10,
         borderRadius: 999,
-    }
+    },
+    passwordContainer: {
+        flexDirection: 'row',
+        marginTop: 50,
+        borderBottomWidth: 1,
+        fontSize: 16,
+        paddingVertical: 8,
+    },
+    passwordInput: {
+        flex: 1,
+        fontSize: 16,
+    },
 })
